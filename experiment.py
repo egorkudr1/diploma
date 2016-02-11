fix bug in CLiMF - incorrect calculation of the derivative, add climf author experiment

experiment.py
@@ -1,18 +1,20 @@
 import numpy as np
 import rankingmeasure
 import sys
+import pickle
+from scipy.sparse import csr_matrix
 
 
-def all_measures(train, test, method, skip_train=False, top=None):
-    print("precision5", rankingmeasure.get_prec_K(5, train, test, method, skip_train=skip_train, top=top))
+def all_measures(train, test, method, skip_train=False, top=None, K=5):
+    print("precision5", rankingmeasure.get_prec_K(train, test, method, skip_train=skip_train, top=top, K=K))
     sys.stdout.flush()
-    print("1-call5", rankingmeasure.get_one_recal_K(5, train, test, method, skip_train=skip_train, top=top))
+    print("1-call5", rankingmeasure.get_one_recal_K(train, test, method, skip_train=skip_train, top=top, K=K))
     sys.stdout.flush()
-    print("MRR", rankingmeasure.get_MRR(train, test, method, skip_train=skip_train, top=top))
+    print("MRR", rankingmeasure.get_MRR(train, test, method, skip_train=skip_train, top=top, K=K))
     sys.stdout.flush()
-    print("AUC", rankingmeasure.get_AUC(train, test, method, skip_train=skip_train, top=top))
+    print("AUC", rankingmeasure.get_AUC(train, test, method, skip_train=skip_train, top=top, K=K))
     sys.stdout.flush()
-    print("NGDC", rankingmeasure.get_NDCG(train, test, method, skip_train=skip_train, top=top))
+    print("NGDC", rankingmeasure.get_NDCG(train, test, method, skip_train=skip_train, top=top, K=K))
     sys.stdout.flush()
 
 
@@ -58,12 +60,25 @@ def givenK_train_test(data, K):
     return (train, test, xlist)
 
 
+def create_csr(data, user_item, file_name):
+    inf = []
+    row = []
+    col = []
+    for u, items in enumerate(data):
+        for i in items:
+            inf.append(1)
+            row.append(u)
+            col.append(i)
+    new_data = csr_matrix((inf,(row, col)), shape=user_item).astype('float')
+    with open(file_name, 'wb') as f:
+        pickle.dump(new_data, f, 0)
+#     return new_data
+
+
+
 def create_original_sample(data, index):
     res = []
     for i in index:
         res.append(data[i])
     return res
 
-
-def wow():
-    return "Wowsds"

rankingmeasure.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.metrics import roc_auc_score
 
 
-def get_prec_K(K, train, test,  cls,  skip_train=False, top=None):
+def get_prec_K(train, test,  cls,  skip_train=True, top=None, K=5):
     res = 0
     for u, (test_cur, train_cur) in enumerate(zip(test, train)):
         ulist = cls.get_list(u)
@@ -25,7 +25,7 @@ def get_prec_K(K, train, test,  cls,  skip_train=False, top=None):
     return res
 
 
-def get_one_recal_K(K, train, test,  cls,  skip_train=False, top=None):
+def get_one_recal_K(train, test,  cls,  skip_train=True, top=None, K=5):
     res = 0
     for u, (test_cur, train_cur) in enumerate(zip(test, train)):
         ulist = cls.get_list(u)
@@ -47,7 +47,7 @@ def get_one_recal_K(K, train, test,  cls,  skip_train=False, top=None):
     return res
 
 
-def get_RR(train, test, ulist, skip_train=False, top=None):
+def get_RR(train, test, ulist, skip_train=True, top=None):
     n_skip = 0 
     for rank, item in enumerate(ulist):
         if top is not None and item in top:
@@ -62,7 +62,7 @@ def get_RR(train, test, ulist, skip_train=False, top=None):
     return 0
 
 
-def get_MRR(train, test,  cls, skip_train=False, top=None):
+def get_MRR(train, test,  cls, skip_train=True, top=None, K=5):
     res = 0
     for u, (test_cur, train_cur) in enumerate(zip(test, train)):
         ulist = cls.get_list(u)
@@ -72,31 +72,27 @@ def get_MRR(train, test,  cls, skip_train=False, top=None):
     return res
 
 
-def get_AUC(train, test, cls, skip_train=False, top=None, K=5):
+def get_AUC(train, test, cls, skip_train=True, top=None, K=5):
     res = 0
-    ans = np.arange(K)[::-1]
     for u, (test_cur, train_cur) in enumerate(zip(test, train)):
         ulist = cls.get_list(u)
         if  skip_train:
             mask = np.in1d(ulist, train_cur)
             ulist = ulist[~mask]
-        localK = min(K, ulist.shape[0])
-        mask = np.in1d(ulist[:localK], test_cur)
+        mask = np.in1d(ulist, test_cur)
         if not skip_train:
-            mask2 = np.in1d(ulist[:localK], train_cur)
+            mask2 = np.in1d(ulist, train_cur)
             mask = np.logical_or(mask, mask2)
         if top is not None:
-            masktmp = np.in1d(ulist[:localK], top)
+            masktmp = np.in1d(ulist, top)
             mask = np.logical_and(mask, np.logical_not(masktmp))
-        if (mask == True).all():
-            res += 1
-        elif not (mask == False).all():
-            res += roc_auc_score(mask, ans[:localK])
+
+        res += roc_auc_score(mask, np.arange(ulist.shape[0])[::-1])
     res /= len(test)
     return res
 
 
-def get_NDCG(train, test, cls, skip_train=False, top=None, K=5):
+def get_NDCG(train, test, cls, skip_train=True, top=None, K=5):
     res = 0
     D = 1 / np.log(np.arange(K) + 2)
     maxNDCG = np.sum(D)

rankingmethod.py
@@ -43,11 +43,12 @@ class CLiMF:
         self.verbose = verbose
         self.N_users = user_item[0]
         self.N_items = user_item[1]
-        self.U = 0.01 * np.random.random((self.N_users, self.K))
-        self.V = 0.01 * np.random.random((self.N_items, self.K))
+        
 
 
     def fit(self, data):
+        self.U = 0.01 * np.random.random((self.N_users, self.K))
+        self.V = 0.01 * np.random.random((self.N_items, self.K))
         for t in range(self.maxiter):
             for u, items in enumerate(data):
                 curU = self.U[u]
@@ -75,7 +76,7 @@ class CLiMF:
                     denominanor  = 1 / (1 - expit(diff_f_ij))
                   
                     coef = np.sum(diverV[j, :] * (denominanor[:, j] - denominanor[j, :]))
-                    dV = coef  * self.U[u] + g_ij[j] - self.reg * curV[j]
+                    dV = (coef + g_ij[j]) * self.U[u]  - self.reg * curV[j]
                     self.V[index] += self.lrate * dV
                 
             if self.verbose == 1:
@@ -112,12 +113,12 @@ class BPR_MF:
         self.maxiter = maxiter
         self.N_users = user_item[0]
         self.N_items = user_item[1]
-        self.U = 0.1 * np.random.random((self.N_users, self.K))
-        self.V = 0.1 * np.random.random((self.N_items, self.K))
         self.verbose = verbose
 
 
     def fit(self, data):
+        self.U = 0.1 * np.random.random((self.N_users, self.K))
+        self.V = 0.1 * np.random.random((self.N_items, self.K))
         num_pos_feedback = 0
         for items in data:
             num_pos_feedback += len(items)
@@ -172,11 +173,12 @@ class iMF:
         self.alpha = alpha
         self.maxiter = maxiter
         self.verbose = verbose
-        self.U = 0.01 * np.random.random((self.N_users, self.K))
-        self.V = 0.01 * np.random.random((self.N_items, self.K))
+        
 
 
     def fit(self, data):
+        self.U = 0.01 * np.random.random((self.N_users, self.K))
+        self.V = 0.01 * np.random.random((self.N_items, self.K))
         itemdata = [[] for i in range(self.N_items)]
         for u, items in enumerate(data):
             for i in items: