f08ba596bd2c6ed7948e82ab68d87d64ed25c7ce
George Kudryavtsev <egorkudr1@gmail.com>
2016-06-11 12:15:12 +0300

    delete extra files

--- rankensemble.py
+++ rankensemble.py
@@ -8,52 +8,6 @@ import random
 import rankingmeasure
 
 
-# Это класс, который хранит алгоритмы ранжирования вместе.
-# Позволяет хранить методы в обученном состоянии, а также проводить некоторый аналитику
-class ensemble:
-    def __init__(self, list_cf, verbose=0):
-        self.list_cf = copy.deepcopy(list_cf)
-        self.verbose = verbose
-    
-    #обучение всех алгоритмов обучения
-    def fit(self, data):
-        for i in range(len(self.list_cf)):
-            self.list_cf[i].fit(data)
-            if self.verbose == 1:
-                print("iteration", i)
-                sys.stdout.flush()
-    
-    #показ первых topK предметов, которые выдал каждый из алгоритмов ранжирования
-    def show_all_ulist(self, u, topK=5):
-        for i in range(len(self.list_cf)):
-            print(type(self.list_cf[i]).__name__, self.list_cf[i].get_list(u)[:topK])
-
-    #попарное сравнение первых topK множеств предметов, которые выдл каждый 
-    #из алгоритмов ранжирования, для пользователя u.
-    #не учитывает поряд.
-    def matrix_similar(self, u, topK=5):
-        res = []
-        for i in range(len(self.list_cf)):
-            res.append(self.list_cf[i].get_list(u)[:topK])
-
-        ans = np.zeros((len(res), len(res)))
-        for i in range(ans.shape[0]):
-            for j in range(i + 1, ans.shape[1]):
-                ans[i, j] = np.mean(np.in1d(res[i], res[j]))
-                ans[j, i] = ans[i, j]
-        return ans
-
-    #Уредненное значение метода matrix_similar по всем пользователям.
-    def mean_matrix_similar(self, topK=5):
-        N_users = self.list_cf[0].N_users
-        for u in range(N_users):
-            if u == 0:
-                ans = matrix_similar(self, u, topK)
-            else:
-                ans += matrix_similar(self, u, topK)
-        ans /= N_users
-        return ans
-
  
 class top_ensemble:
     def __init__(self, topK = 15, verbose=0):
@@ -246,69 +200,6 @@ class tree_val_ens:
     def get_list(self, u):
         return self.res_model.get_list(u)
 
-# def find_alpha(a1, a2, index = None):
-#     if index is None:
-#         all_pairs = np.transpose([np.tile(a1, a2.shape[0]), np.tile(a2, a1.shape[0]), 
-#                       np.repeat(a1, a2.shape[0]), np.repeat(a2, a1.shape[0])])
-#     else:
-#         all_pairs = np.transpose([np.tile(a1[index], a2.shape[0]), np.tile(a2[index], a1.shape[0]), 
-#                       np.repeat(a1, len(index)), np.repeat(a2, len(index))])
-#     # print(all_pairs.shape, all_pairs)
-#     alpha = (- all_pairs[:, 1]  + all_pairs[:, 3]) / (all_pairs[:, 0] - all_pairs[:, 1] - all_pairs[:, 2]  + all_pairs[:, 3])
-#     alpha = alpha[np.logical_not(np.isnan(alpha))]
-#     alpha = alpha[alpha > 0 ]
-#     alpha = alpha[alpha < 1]
-#     return np.unique(alpha)
-
-
-# class smart_val_ens:
-#     def __init__(self, maxn = 1000, topK = 5, verbose=0):
-#         self.maxn = maxn
-#         self.verbose = verbose
-#         self.topK = topK
-
-
-#     def fit(self, list_cf, validation, train, trainvalidation):
-#         self.list_cf = copy.deepcopy(list_cf)
-#         self.N_items = self.list_cf[0].N_items
-#         self.N_users = self.list_cf[0].N_users
-
-
-#         alpha = []
-#         all_items = np.arange(self.N_items)
-#         for u in range(self.N_users):
-#             rank1 = self.list_cf[0].get_list(u)[:self.topK]
-#             rank2 = self.list_cf[1].get_list(u)[:self.topK]
-
-#             f1 = self.list_cf[0].get_f(u)
-#             f2 = self.list_cf[1].get_f(u)
-
-#             changed_index2 = f2 >= np.min(f2[rank1])
-#             changed_index1 = f1 >= np.min(f1[rank2])
-
-#             f1_1 = f1[changed_index1]
-#             f2_1 = f2[changed_index1]
-#             rank1 = np.argsort(-f1_1)
-
-#             f1_2 = f1[changed_index2]
-#             f2_2 = f2[changed_index2]
-#             rank2 = np.argsort(-f2_2)   
-
-#             totaln1 = min(np.sum(changed_index1), self.maxn)
-#             totaln2 = min(np.sum(changed_index2), self.maxn)
-
-#             l1 = find_alpha(f1_1, f2_1, rank1) 
-#             l2 = find_alpha(f2_2, f1_2, rank2)
-#             print(l1, l2)
-#             print(l1.shape, l2.shape)
-#             loc_alpha = np.unique(loc_alpha)
-#             if self.verbose == 2:
-#                 print(u, loc_alpha.shape, len(alpha))
-#                 sys.stdout.flush()
-#             alpha.expand(loc_alpha)
-
-
-
 
 class regression_ensemble:
     def __init__(self, model, tiedrank = False, ratio_neg = 5, verbose=0):
@@ -319,12 +210,6 @@ class regression_ensemble:
 
 
     def fit(self, list_cf, validation, train, trainvalidation):
-        # numpos = 0
-        # for items in validation:
-        #     numpos += len(items)
-        # numpos *= len(list_cf)
-        # X = np.zeros((numpos * (1 + ratio_neg), len(list_cf)))
-        # y = np.zeros((numpos * (1 + ratio_neg), len(list_cf)))
         self.list_cf = copy.deepcopy(list_cf)
         self.N_items = self.list_cf[0].N_items
         self.N_users = len(validation)
@@ -373,11 +258,7 @@ class regression_ensemble:
         fij -= np.min(fij, axis = 0)[np.newaxis, :]
         fij /= np.max(fij, axis = 0)[np.newaxis, :]
 
-        # print(fij.shape)
-        # print(self.N_items)
-        # res = self.model.predict_proba(fij)[1]
         res = self.model.predict(fij)
-        # print(res.shape)
         return np.argsort(-res)
 
 
